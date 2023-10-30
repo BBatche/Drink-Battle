c4bb36790d308c52e186fe48337d84af06724913
BBatche <bcb248@gmail.com>
2023-10-30 12:19:29 -0400

    game created

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Netcode;
+using UnityEngine;
+
+public class PlayerMovementScript : NetworkBehaviour
+{
+    float xDirection = 0.0f;
+    float yDirection = 0.0f;
+
+    float moveSpeed = 2.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        xDirection = Input.GetAxisRaw("Horizontal");
+        yDirection = Input.GetAxisRaw("Vertical");
+
+        Vector3 move = new Vector3(xDirection, yDirection, 0);
+
+        transform.Translate(move * Time.deltaTime * moveSpeed);
+
+    }
+
+    public override void OnNetworkSpawn()
+    {
+        base.OnNetworkSpawn();
+        if (!IsOwner)
+        {
+            enabled = false;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        
+    }
+
+
+}
